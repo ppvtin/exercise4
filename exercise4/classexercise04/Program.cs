bd7563d77d8f1eec94b04d330c412dc88a621314
bootrang9680 <bootrang090999@gamil.com>
2019-09-26 14:28:14 +0700

    D:\exercise4

---
+++ exercise4/classexercise04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classexercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello word");
+        }
+
+    }
+}
