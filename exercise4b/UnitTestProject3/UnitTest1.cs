6b38cb3c6fbd28a9b796e308d1c4e1fa2ba5c7cf
trongtam <supercubts000@gmail.com>
2019-10-01 15:28:28 +0700

    codetui

---
+++ exercise4b/UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using exercise4b;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ exercise4b/exercise4b/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4b
+{
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[10];
+            InputInt(Arr);
+            Console.ReadLine();
+        }
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                string str = Console.ReadLine();
+                Arr[i] = int.Parse(str);
+            }
+        }
+    }
+}
