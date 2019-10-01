acd37ef71da5a5af235e206cbeb170aaccaebb64
trongtam <supercubts000@gmail.com>
2019-10-01 15:29:42 +0700

    codetui

---
+++ exercise04/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using exercise04;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOdd()
+        {
+            bool result2 = Program.IsOdd(2);
+            Assert.AreEqual(false, result2);
+
+            bool result5 = Program.IsOdd(5);
+            Assert.AreEqual(true, result5);
+        }
+        [TestMethod]
+        public void TestEven()
+        {
+            bool result2 = Program.IsEven(2);
+            Assert.AreEqual(true, result2);
+
+            bool result5 = Program.IsEven(5);
+            Assert.AreEqual(false, result5);
+        }
+
+        [TestMethod]
+        public void TestPrime()
+        {
+            bool result3 = Program.IsPrime(13);
+            Assert.AreEqual(true, result3);
+            bool result4 = Program.IsPrime(4);
+            Assert.AreEqual(false, result4);
+        }
+        [TestMethod]
+        public void pow()
+        {
+            int result2 = Program.pow(2, 3);
+            Assert.AreEqual(8, result2);
+
+            int result3 = Program.pow(3, 0);
+            Assert.AreEqual(1, result3);
+
+            int resultAm7 = Program.pow(2, -7);
+            Assert.AreEqual(0, resultAm7);
+        }
+        [TestMethod]
+        public void Abs()
+        {
+            int resultAm2 = Program.Abs(-2);
+            Assert.AreEqual(2, resultAm2);
+
+            int result3 = Program.Abs(3);
+            Assert.AreEqual(3, result3);
+
+            int result0 = Program.Abs(0);
+            Assert.AreEqual(0, result0);
+        }
+        [TestMethod]
+        public void Ceil()
+        {
+            int resultAm0_95 = Program.Ceil(-0.95f);
+            Assert.AreEqual(0, resultAm0_95);
+
+            int result7_004 = Program.Ceil(7.004f);
+            Assert.AreEqual(8, result7_004);
+
+            int resultam4 = Program.Ceil(-4);
+            Assert.AreEqual(-4, resultam4);
+        }
+        [TestMethod]
+        public void Floor()
+        {
+            int resultAm0_95 = Program.Floor(-0.95f);
+            Assert.AreEqual(-1, resultAm0_95);
+
+            int result7_004 = Program.Floor(7.004f);
+            Assert.AreEqual(7, result7_004);
+
+            int resultam4 = Program.Floor(-4);
+            Assert.AreEqual(-4, resultam4);
+        }
+
+        [TestMethod]
+        public void Factorial()
+        {
+            long result2 = Program.Factorial(2);
+            Assert.AreEqual(2, result2);
+
+            long result3 = Program.Factorial(3);
+            Assert.AreEqual(6, result3);
+
+            long result0 = Program.Factorial(0);
+            Assert.AreEqual(1, result0);
+
+        }
+        
+    }
+}
---
+++ exercise04/exercise04/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise04
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("xin mời nhập n");
+                    long n = long.Parse(Console.ReadLine());
+                    Console.WriteLine("xin mời nhập x");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("xin mời nhập y");
+                    double y = double.Parse(Console.ReadLine());
+                    UserInput(n, x, y);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("bạn đã nhập dữ liệu sai: ");
+                    Console.WriteLine("n thuộc kiểu long ");
+                    Console.WriteLine("x thuộc kiểu double");
+                    Console.WriteLine("y thuộc kiểu double");
+                    Console.WriteLine("vui lòng nhập lại");
+                    Console.WriteLine("----------------------------------------------------------------");
+                }
+            }
+            Console.ReadLine();
+
+        }
+        public static void UserInput(long n, double x, double y)
+        {
+            Console.WriteLine(IsEven((int) n));
+            Console.WriteLine(IsPrime((int)n));
+            Console.WriteLine(Square((int) n));
+            Console.WriteLine(Cube((int) n));
+            Console.WriteLine(pow((int)x, (int)y));
+            Console.WriteLine(Abs((int)n));
+            Console.WriteLine(Ceil((float)x));
+            Console.WriteLine(Floor((float)x));
+            Console.WriteLine(Rand1());
+            Console.WriteLine(Rand2());
+            Console.WriteLine(Factorial(n));
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+
+        public static bool IsOdd(int n)
+        {
+            if (n % 2 != 0)
+                return true;
+            else return false;
+        }
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else return false;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n == 1) return true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static int Square(int n)
+        {
+
+            return n * n;
+        }
+
+        public static int Cube(int n)
+        {
+            return n * n * n;
+        }
+
+        public static int pow(int x, int y)
+        {
+            Console.Write("pow của {0} và {1}: ", x, y);
+            int kq = x;
+            if (y < 0)
+            {
+                return 0;
+            }
+            if (y == 0)
+            {
+                return 1;
+            }
+            if (y == 1) return kq;
+            for (int i = 2; i <= y; i++)
+            {
+                kq *= x;
+            }
+            return kq;
+        }
+
+        public static int Abs(int n)
+        {
+            Console.Write("Abs của {0}: ", n);
+            if (n < 0)
+            {
+                return -n;
+            }
+            else return n;
+        }
+        //int: float x
+        //Action: x > kq --> kp+=1
+        //out: kp+=1 else int x
+        public static int Ceil(float x)
+        {
+            Console.Write("Ceil của {0}: ", x);
+            int kq = (int)x;
+            if (x > kq) return kq += 1;
+            else return kq;
+
+        }
+
+        public static int Floor(float x)
+        {
+            Console.Write("Floor của {0}: ", x);
+            int kq = (int)x;
+            if (x >= kq) return kq;
+            else return kq - 1;
+        }
+
+        public static int Rand1()
+        {
+            int kq = 2;
+            int kq2;
+            for (int i = 2; i <= 31; i++)
+            {
+                kq *= 2;
+            }
+            Random rd = new Random();
+            Console.Write("random từ 0 đến (2^31)-1: ");
+            return kq2 = rd.Next(0, kq - 1);
+        }
+
+        public static float Rand2()
+        {
+            Random rd = new Random();
+            float f = (float)rd.NextDouble();
+            Console.Write("random từ giữa 0 đến 1: ");
+            return f;
+
+        }
+        public static long Factorial(long n)
+        {
+            Console.Write("Factorial của {0}: ", n);
+            long kq = 1;
+            if (n == 0) return 1;
+            else if (n < 0)
+            {
+                Console.Write("kq ra 0 => vô nghiệm ,");
+                return 0;
+            }
+            else
+            {
+                for (long i = 1; i <= n; i++)
+                {
+                    kq *= i;
+                }
+                return kq;
+            }
+        }
+       
+    }
+
+}
